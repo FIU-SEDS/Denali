b1a15040cd5857a727d3436ebbfcb6da7736466c
Nathan <martineznathan1010@gmail.com>
2025-12-23 14:00:01 -0500

    serialization for FILE/RADIO

--- include/sensors.h
+++ include/sensors.h
@@ -1,21 +1,26 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 
+#include <serial.h>
+
 #define CS_BMI088_GYRO 0
 #define CS_BMI088_ACCL
 
 // TEMPORARY CHIP SELECT DEFINES HERE FOR GIT CONVENIENCE. CHANGE LATER
 
 struct IMU_data {
+  const sensor_ID ID = IMU_ID;
   float acl_x, acl_y, acl_z;
   float gyro_x, gyro_y, gyro_z;
 };
 
 struct BME_data {
+  const sensor_ID ID = BME_ID;
   float temp, humidity, pressure, altitude;
 };
 
 struct GPS_data {
+  const sensor_ID ID = GPS_ID;
   float latitude, longitude;
 };
 
