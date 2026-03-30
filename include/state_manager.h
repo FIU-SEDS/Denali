c66c56e5ee2b656017f9e178536e9ab081a6e237
Matthew Quintero <matthewquintero1@yahoo.com>
2026-03-30 17:45:53 -0400

    cleaned up update rocket state function

--- include/state_manager.h
+++ include/state_manager.h
@@ -24,6 +24,9 @@ enum sensors : uint8_t
   ROCKET_STATE
 };
 
+//temp for compile purposes of log state change
+extern enum rocket_state rocket;
+
 
 rocket_state update_rocket_states(rocket_state state, GPS_data &gps_data, BME_data &bme_data, IMU_data &imu_data, MAG_data &mag_data);
 
